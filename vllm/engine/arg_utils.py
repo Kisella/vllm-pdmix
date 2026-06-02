571835facdc5bebab8418720e84eeb1f7ab202d6
Kisella <yezhujun1@h-partners.com>
2026-06-02 17:43:39 +0800

    PD batch separation

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -595,6 +595,9 @@ class EngineArgs:
     disable_chunked_mm_input: bool = SchedulerConfig.disable_chunked_mm_input
 
     scheduler_reserve_full_isl: bool = SchedulerConfig.scheduler_reserve_full_isl
+    
+    enable_pd_separation: bool = SchedulerConfig.enable_pd_separation
+    pd_scheduling_policy: str = SchedulerConfig.pd_scheduling_policy
 
     disable_hybrid_kv_cache_manager: bool | None = (
         SchedulerConfig.disable_hybrid_kv_cache_manager
@@ -1393,6 +1396,12 @@ class EngineArgs:
         scheduler_group.add_argument(
             "--stream-interval", **scheduler_kwargs["stream_interval"]
         )
+        scheduler_group.add_argument(
+            "--enable-pd-separation", **scheduler_kwargs["enable_pd_separation"]
+        )
+        scheduler_group.add_argument(
+            "--pd-scheduling-policy", **scheduler_kwargs["pd_scheduling_policy"]
+        )
 
         # Compilation arguments
         compilation_kwargs = get_kwargs(CompilationConfig)
@@ -2000,6 +2009,8 @@ class EngineArgs:
             disable_hybrid_kv_cache_manager=self.disable_hybrid_kv_cache_manager,
             async_scheduling=self.async_scheduling,
             stream_interval=self.stream_interval,
+            enable_pd_separation=self.enable_pd_separation,
+            pd_scheduling_policy=self.pd_scheduling_policy,
         )
 
         if not model_config.is_multimodal_model and self.default_mm_loras:
