29d4264df97ee4e1925d2a9d455daa720d08381c
Kisella <yezhujun1@h-partners.com>
2026-06-12 10:21:35 +0800

    Edge-Cloud: PD batch separation. fix-slice

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -982,8 +982,7 @@ class GroupCoordinator:
             if tensor.is_cuda:
                 tensor.record_stream(torch.cuda.current_stream(tensor.device))
             elif tensor.device.type == "npu":
-                import torch_npu
-                tensor.record_stream(torch_npu.current_stream(tensor.device))
+                tensor.record_stream(torch.npu.current_stream(tensor.device))
             handles.append(handle)
 
         return handles
